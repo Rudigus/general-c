42ce9826d2d3840d246bd05707b11f7a2913fc9b
Rudigus <rodrigo.matos9@hotmail.com>
2020-01-31 12:27:38 -0300

    questao 1 nao funcionando ainda

--- listas/lista_01/lista_01_questao_1.c
+++ listas/lista_01/lista_01_questao_1.c
@@ -2,5 +2,28 @@
 
 // URI Online Judge - Problema 1176 - Fibonacci Array - Nível 4
 
-int main(){}
+// Função que recebe como parâmetro um número N e retorna o N-ésimo termo da sequência de Fibonacci.
+unsigned long fibonacci(int numFornecido)
+{
+	unsigned long n1 = 0, n2 = 1, aux = 0;
+	for(int i = 0; i < numFornecido; i++)
+	{
+		aux = n2;
+		n2 = n2 + n1;
+		n1 = aux;
+	}
+	return n1;
+}
 
+// Lê um número X como entrada e executa X testes. Cada teste consiste em printar o resultado da função Fibonacci de acordo com a entrada fornecida.
+int main()
+{
+	int numeroTestes;
+	scanf("%d", &numeroTestes);
+	for(int i = 0; i < numeroTestes; i++)
+	{
+		int numFornecido;
+		scanf("%d", &numFornecido);
+		printf("Fib(%d) = %lu\n", numFornecido, fibonacci(numFornecido));
+	}
+}
