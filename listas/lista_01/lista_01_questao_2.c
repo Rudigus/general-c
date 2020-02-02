7bc06c0c30e454638569b4af8b7bda71a53b12db
Rudigus <rodrigo.matos9@hotmail.com>
2020-02-02 16:21:25 -0300

    questao 3 feita

--- listas/lista_01/lista_01_questao_2.c
+++ listas/lista_01/lista_01_questao_2.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
 // URI Online Judge - Problema 1180 - Lowest Number and Position - Nível 2
+// Aluno: Rodrigo Matos Aguiar - Rudigus
 
-int main(){}
+// Recebe uma array e retorna o índice do número de menor valor de toda a array.
+int calcularIndiceMenorValor(int x[], int n)
+{
+	int imv = 0;
+	for(int i = 1; i < n; i++)
+	{
+		if(x[i] < x[imv])
+		{
+			imv = i;
+		}
+	}
+	return imv;
+}
+
+// Recebe um inteiro X informando o tamanho de uma array, recebe X inteiros que a compõem e
+// informa o elemento de menor valor e seu respectivo índice.
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	int x[n];
+	for(int i = 0; i < n; i++)
+	{
+		scanf("%d", &x[i]);
+	}
+	int imv = calcularIndiceMenorValor(x, n);
+	printf("Menor valor: %d\n", x[imv]);
+	printf("Posicao: %d\n", imv);
+}
 
--- listas/lista_01/lista_01_questao_3.c
+++ listas/lista_01/lista_01_questao_3.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
 
 // URI Online Judge - Problema 1182 - Column in Array - Nível 3
+// Aluno: Rodrigo Matos Aguiar - Rudigus
 
-int main(){}
+// Tamanho da matriz quadrada
+int t = 12;
 
+// Calcula a soma de todos os 144 elementos da matriz
+float sum(float matrix[t][t], int column)
+{
+	float sum = 0;
+	for(int i = 0; i < t; i++)
+	{
+		sum += matrix[i][column];
+		printf("matrix[%d][%d] = %f\n", i, column, matrix[i][column]);
+	}
+	return sum;
+}
+
+// Calcula a média entre todos os 144 elementos da matriz
+float average(float matrix[t][t], int column)
+{
+	float average = 0;
+	for(int i = 0; i < t; i++)
+	{
+		average += matrix[i][column];
+	}
+	average /= t;
+	return average;
+}
+
+// Calcula a soma ou a média de elementos de uma coluna de uma matriz e mostra esse resultado, com uma casa decimal.
+int main()
+{
+	float matrix[t][t];
+	int c;
+	scanf("%d", &c);
+	char operation;
+	scanf(" %c", &operation);
+	for(int i = 0; i < t*t; i++)
+	{
+		scanf("%f", &matrix[i/t][i%t]);
+	}
+	printf("%.1f\n", (operation == 'S') ? sum(matrix, c) : average(matrix, c));
+}
