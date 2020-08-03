4288c2f9d5d03612acd5d4cf832c9f27bef027d1
Rudigus <rodrigo.matos9@hotmail.com>
2020-08-03 02:22:11 -0300

    first question solved, but still 30% wa in uri

---
+++ exercicios/lista_05/lista_05_questao_1.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define true 1
+#define false 0
+
+typedef struct fila
+{
+    int chave;
+    struct fila *proximo;
+} fila;
+
+fila* inicializarFila()
+{
+    fila *fim = (fila*) malloc(sizeof(fila));
+    fim->chave = 1;
+    fim->proximo = NULL;
+    return fim;
+}
+
+void enfila(fila** fim, int chave)
+{
+    fila *f = inicializarFila();
+    f->chave = chave;
+    f->proximo = *fim;
+    *fim = f;
+}
+
+void desenfila(fila** fim)
+{
+    fila *f = *fim;
+    while(f->proximo->proximo != NULL)
+    {
+        f = f->proximo;
+    }
+    fila *temp = f->proximo;
+    f->proximo = NULL;
+    free(temp);
+}
+
+fila* acharInicio(fila* fim)
+{
+    fila *inicio = fim;
+    while(inicio->proximo != NULL)
+    {
+        inicio = inicio->proximo;
+    }
+    return inicio;
+}
+
+//int calcularNumeroCartas(fila *fim)
+//{
+//    int i = 1;
+//    fila *f = fim;
+//    while(f->proximo != NULL)
+//    {
+//        i++;
+//        f = f->proximo;
+//    }
+//    return i;
+//}
+
+int main()
+{
+    while(true) {
+        int numeroCartas;
+        scanf("%d", &numeroCartas);
+        if(numeroCartas == 0)
+        {
+            break;
+        }
+        printf("Discarded cards:");
+        fila *fim = inicializarFila();
+        for(int i = 0; i < numeroCartas - 1; i++)
+        {
+            enfila(&fim, i + 2);
+        }
+        while(numeroCartas >= 2)
+        {
+            fila *inicio = acharInicio(fim);
+            printf(" %d", inicio->chave);
+            desenfila(&fim);
+            numeroCartas--;
+            if(numeroCartas >= 2)
+            {
+                printf(",");
+                inicio = acharInicio(fim);
+                desenfila(&fim);
+                enfila(&fim, inicio->chave);
+            }
+        }
+        printf("\nRemaining card: %d\n", fim->chave);
+    }
+}
