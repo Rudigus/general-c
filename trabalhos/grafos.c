b23f423cc8ed1cb71b952f1450529d1666a0ddd3
Rudigus <rodrigo.matos9@hotmail.com>
2020-09-17 14:54:43 -0300

    backup grafos

--- trabalhos/grafos.c
+++ trabalhos/grafos.c
@@ -1,5 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct adjacencia {
+    int vertice;
+    struct adjacencia *prox;
+} adjacencia;
+
+typedef struct vertice {
+    adjacencia *cabeca;
+} vertice;
+
+typedef struct grafo {
+    int numeroVertices;
+    int numeroArestas;
+    vertice *vertices;
+} grafo;
+
+grafo* inicializaGrafo(int numeroVertices, int numeroArestas) {
+    grafo *temp = (grafo*) malloc(sizeof(grafo));
+    temp->numeroVertices = numeroVertices;
+    temp->numeroArestas = numeroArestas;
+    temp->vertices = (vertice*) malloc(numeroVertices * sizeof(vertice));
+    for(int i = 0; i < numeroVertices; i++) {
+        temp->vertices[i].cabeca = NULL;
+    }
+    return temp;
+}
 
 int main() {
-    printf("opa");
+    int numeroCasos;
+    scanf("%d", &numeroCasos);
+    for(int i = 0; i < numeroCasos; i++) {
+        int nodoDesejado, numeroVertices, numeroArestas;
+        scanf("%d", &nodoDesejado);
+        scanf("%d %d", &numeroVertices, &numeroArestas);
+        grafo *labirinto = inicializaGrafo(numeroVertices, numeroArestas);
+        for(int j = 0; j < numeroArestas; j++) {
+            
+        }
+    }
 }
