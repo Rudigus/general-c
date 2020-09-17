d6e3be53bcdbe44d3666869c2d52cf23693533c5
Rudigus <rodrigo.matos9@hotmail.com>
2020-09-17 15:10:14 -0300

    all input was obtained

--- trabalhos/grafos.c
+++ trabalhos/grafos.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// URI Online Judge
+// 1076 - Desenhando Labirintos
+
 typedef struct adjacencia {
     int vertice;
     struct adjacencia *prox;
@@ -27,6 +30,19 @@ grafo* inicializaGrafo(int numeroVertices, int numeroArestas) {
     return temp;
 }
 
+adjacencia* criaAdjacencia(int vertice) {
+    adjacencia *temp = (adjacencia*) malloc(sizeof(adjacencia));
+    temp->vertice = vertice;
+    temp->prox = NULL;
+    return temp;
+}
+
+void criaAresta(grafo *labirinto, int verticePartida, int verticeDestino) {
+    adjacencia *adj = criaAdjacencia(verticeDestino);
+    adj->prox = labirinto->vertices[verticePartida].cabeca;
+    labirinto->vertices[verticePartida].cabeca = adj;
+}
+
 int main() {
     int numeroCasos;
     scanf("%d", &numeroCasos);
@@ -36,7 +52,8 @@ int main() {
         scanf("%d %d", &numeroVertices, &numeroArestas);
         grafo *labirinto = inicializaGrafo(numeroVertices, numeroArestas);
         for(int j = 0; j < numeroArestas; j++) {
-            
+            int verticePartida, verticeDestino;
+            scanf("%d %d", &verticePartida, &verticeDestino);
         }
     }
 }
