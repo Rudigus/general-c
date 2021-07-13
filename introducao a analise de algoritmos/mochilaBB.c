4c422520b870dbdd518000b218415a3abca3c979
Rudigus <rodrigo.matos9@hotmail.com>
2021-07-12 21:38:56 -0300

    adds mochilaBB and texam

--- introducao a analise de algoritmos/mochilaBB.c
+++ introducao a analise de algoritmos/mochilaBB.c
@@ -9,63 +9,6 @@ typedef struct {
     int quantidade;
 } MOCHILA;
 
-/* Se houver memória disponível, devolve o valor de uma solução ótima para a instância do PMB armazenada em p; caso contrário, devolve -1 */
-double pmbPD(MOCHILA p) {
-    int i, j;
-    double *x, *ant, *aux;
-    x = malloc(sizeof(double) * (p.capacidade + 1));
-    ant = calloc(p.capacidade + 1, sizeof(double));
-    if (x == NULL || ant == NULL) {
-        return -1;
-    }
-    for (j = 0; j < p.quantidade; j++) {
-        for (i = 0; i <= p.capacidade; i++) {
-            if (p.peso[j] <= i && p.valor[j] + ant[i - (int) p.peso[j]] > ant[i]) {
-                x[i] = p.valor[j] + ant[i - (int) p.peso[j]];
-            } else {
-                x[i] = ant[i];
-            }
-        }
-        aux = ant;
-        ant = x;
-        x = aux;
-    }
-    return ant[(int) p.capacidade];
-}
-/* fim pmbPD */
-
-/* se a solução codificada pelos bits i é viável, devolve o valor dessa solução; caso contrário, devolve -1 */
-double valorMochila(MOCHILA p, int i) {
-    int j;
-    double valor = 0, peso = 0;
-    for (j = 0; j < p.quantidade; j++) {
-        if (i % 2) {
-            if (p.peso[j] + peso > p.capacidade) {
-                return -1;
-            }
-            valor += p.valor[j];
-            peso += p.peso[j];
-        }
-        i = i / 2;
-    }
-    return valor;
-}
-/* fim valorMochila */
-
-/* devolve o valor de uma solução ótima para a instância do PMB armazenada em p */
-double pmbEE(MOCHILA p) {
-    int i, n = 2 << p.quantidade;
-    double valor, max = 0;
-    for (i = 1; i < n; i++) {
-        valor = valorMochila(p, i);
-        if (valor > max) {
-            max = valor;
-        }
-    }
-    return max;
-}
-/* fim pmbEE */
-
 /* coloca os itens da mochila em ordem decrescente de valor relativo (valor / peso) */
 void bolha(MOCHILA p) {
     int i, j;
@@ -103,6 +46,19 @@ double pmbBB(MOCHILA p) {
         }
         max = valor > max ? valor : max;
         k = -1;
+        int pesoTotal = 0;
+        int valorTotal = 0;
+        printf("S%d(solução: ", it + 1);
+        for (i = 0; i < p.quantidade; i++) {
+            printf("%d", x[i]);
+            if (x[i]) {
+                valorTotal += p.valor[i];
+                pesoTotal += p.peso[i];
+            }
+        }
+        printf(", valor: %d", valorTotal);
+        printf(", peso: %d)", pesoTotal);
+        printf("\n");
         for (i = p.quantidade - 1; i >= 0; i--) {
             if (x[i]) {
                 capacidade += p.peso[i];
@@ -127,9 +83,11 @@ int main() {
     int i;
     long tempo;
     MOCHILA p;
-    printf("Capacidade da mochila: ");
+    // printf("Capacidade da mochila: ");
+    // fflush(stdout);
     scanf("%lf", &p.capacidade);
-    printf("Quantidade de itens: ");
+    // printf("Quantidade de itens: ");
+    // fflush(stdout);
     scanf("%d", &p.quantidade);
     p.peso = malloc(sizeof(double) * p.quantidade);
     p.valor = malloc(sizeof(double) * p.quantidade);
@@ -138,15 +96,13 @@ int main() {
         return 1;
     }
     for (i = 0; i < p.quantidade; i++) {
-        printf("Peso do item %d: ", i + 1);
+        // printf("Peso do item %d: ", i + 1);
+        // fflush(stdout);
         scanf("%lf", &p.peso[i]);
-        printf("Valor do item %d: ", i + 1);
+        // printf("Valor do item %d: ", i + 1);
+        // fflush(stdout);
         scanf("%lf", &p.valor[i]);
     }
-    tempo = clock();
-    printf("\nSolução pmbPD: %lf (%lf ms)\n", pmbPD(p), (double) (clock() - tempo) / CLOCKS_PER_SEC * 1000);
-    tempo = clock();
-    printf("\nSolução pmbEE: %lf (%lf ms)\n", pmbEE(p), (double) (clock() - tempo) / CLOCKS_PER_SEC * 1000);
     tempo = clock();
     printf("Solução pmbBB: %lf (%lf ms)\n", pmbBB(p), (double) (clock() - tempo) / CLOCKS_PER_SEC * 1000);
     return 0;
