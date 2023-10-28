e9c6695ce5642f102cc755354089f9ebc579b691
Rudigus <rodrigo.matos9@hotmail.com>
2023-10-28 18:33:27 -0300

    tests now use file reading and writing

--- sistemas embarcados/main.c
+++ sistemas embarcados/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <locale.h>
 
-#include "newtonGregory.h"
+#include "newton_gregory.h"
 
 // Declaração das funções
 
--- sistemas embarcados/testes.c
+++ sistemas embarcados/testes.c
@@ -1,77 +1,71 @@
 #include <stdio.h>
 #include <locale.h>
 
-#include "newtonGregory.h"
+#include "newton_gregory.h"
 
-void executarBateriaTestes();
-int testeQuadratica(double xEscolhido);
-int testeCubica(double xEscolhido);
+FILE *abrirArquivoRelatorioTestes();
 
 void main() {
-  executarBateriaTestes();
-}
-
-void executarBateriaTestes() {
-  int numSucessos = 0;
-  int numFalhas = 0;
-  int numTestes = 3;
-  double valoresTeste[] = {2.5, 5, 13.5};
+  int numPontos, numTestes;
+  double x[NG_TAM];
+  double y[NG_TAM];
+  double xDado[NG_TAM];
+  double yEsperado[NG_TAM];
+  double yEncontrado[NG_TAM];
+  double DN[NG_TAM];
+  double O[NG_TAM][NG_TAM];
+  int numAcertos = 0, numErros = 0;
+  FILE *arquivoBateriaTestes;
+  int i;
 
   setlocale(LC_ALL,"Portuguese");
 
-  for (int i = 0; i < numTestes; i++) {
-    numSucessos += testeQuadratica(valoresTeste[i]);
-    numSucessos += testeCubica(valoresTeste[i]);
-  }
-  numFalhas = numTestes * 2 - numSucessos;
+  arquivoBateriaTestes = fopen("bateria_testes.txt", "r");
 
-  printf("\n\nResultado da bateria de testes:\n");
-  printf("\nTestes com sucesso: %d", numSucessos);
-  printf("\nTestes com falha: %d", numFalhas);
+  if (arquivoBateriaTestes == NULL) {
+    FILE *arquivoResultado = abrirArquivoRelatorioTestes();
+    fprintf(arquivoResultado, "Não foi possível ler o arquivo da bateria de testes.\n");
+    fclose(arquivoResultado);
+    return;
+  }
+
+  fscanf(arquivoBateriaTestes, "%d %d", &numPontos, &numTestes);
+  for (i = 0; i < numPontos; i++) {
+    fscanf(arquivoBateriaTestes, "%lf", &x[i]);
+  }
+  for (i = 0; i < numPontos; i++) {
+    fscanf(arquivoBateriaTestes, "%lf", &y[i]);
+  }
+  for (i = 0; i < numTestes; i++) {
+    fscanf(arquivoBateriaTestes, "%lf", &xDado[i]);
+  }
+  for (i = 0; i < numTestes; i++) {
+    fscanf(arquivoBateriaTestes, "%lf", &yEsperado[i]);
+  }
+
+  fclose(arquivoBateriaTestes);
+
+  D(x, y, numPontos, O, DN);
+  FILE *arquivoRelatorio = abrirArquivoRelatorioTestes();
+  fprintf(arquivoRelatorio, "Histórico de testes:\n");
+  for (i = 0; i < numTestes; i++) {
+    yEncontrado[i] = P(x, y, DN, numPontos, xDado[i]);
+    if (yEncontrado[i] == yEsperado[i]) {
+      numAcertos += 1;
+      fprintf(arquivoRelatorio, "\nAcerto, x utilizado: %lf, y encontrado: %lf", xDado[i], yEncontrado[i]);
+    } else {
+      numErros += 1;
+      fprintf(arquivoRelatorio, "\nErro, x utilizado: %lf, y encontrado: %lf, y esperado: %lf", xDado[i], yEncontrado[i], yEsperado[i]);
+    }
+  }
+  fprintf(arquivoRelatorio, "\n\nRelatório da bateria de testes:\n");
+  fprintf(arquivoRelatorio, "\nNúmero de acertos: %d", numAcertos);
+  fprintf(arquivoRelatorio, "\nNúmero de erros: %d", numErros);
+  fclose(arquivoRelatorio);
 }
 
-// Retorna 1 se o teste teve sucesso
-// 0 se o teste falhar
-int testeQuadratica(double xEscolhido) {
-  // Estruturas auxiliares
-  double DN[NG_TAM];
-  double O[NG_TAM][NG_TAM];
-  // Entrada
-  double n = 4;
-  double x[4] = {1, 2, 3, 4};
-  double y[4] = {1, 4, 9, 16};
-
-  D(x, y, n, O, DN);
-  double resultado = P(x, y, DN, n, xEscolhido);
-
-  double yEsperado = xEscolhido * xEscolhido;
-  if (resultado == yEsperado) {
-    printf("\nSucesso, valor: %lf", resultado);
-    return 1;
-  } else {
-      printf("\nFalha, valor retornado: %lf, valor esperado: %lf", resultado, yEsperado);
-    return 0;
-  }
-}
-
-int testeCubica(double xEscolhido) {
-  // Estruturas auxiliares
-  double DN[NG_TAM];
-  double O[NG_TAM][NG_TAM];
-  // Entrada
-  double n = 4;
-  double x[4] = {1, 2, 3, 4};
-  double y[4] = {1, 8, 27, 64};
-
-  D(x, y, n, O, DN);
-  double resultado = P(x, y, DN, n, xEscolhido);
-
-  double yEsperado = xEscolhido * xEscolhido * xEscolhido;
-  if (resultado == yEsperado) {
-    printf("\nSucesso, valor: %lf", resultado);
-    return 1;
-  } else {
-      printf("\nFalha, valor retornado: %lf, valor esperado: %lf", resultado, yEsperado);
-    return 0;
-  }
+FILE *abrirArquivoRelatorioTestes() {
+  FILE *arquivoRelatorioTestes;
+  arquivoRelatorioTestes = fopen("relatorio_testes.txt", "w");
+  return arquivoRelatorioTestes;
 }
